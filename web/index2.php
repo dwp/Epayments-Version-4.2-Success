5a3af806bc48e448bb8152af728cbfdcf57ef33b
Krishan Veerappa <kv02@hotmail.co.uk>
2015-04-27 13:33:55 +0100

    config vars

---
+++ web/index2.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+	* {
+		box-sizing: border-box;
+	}
+		html, body, div, span, applet, object, iframe,
+			h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+			a, abbr, acronym, address, big, cite, code,
+			del, dfn, em, img, ins, kbd, q, s, samp,
+			small, strike, strong, sub, sup, tt, var,
+			b, u, i, center,
+			dl, dt, dd, ol, ul, li,
+			fieldset, form, label, legend,
+			table, caption, tbody, tfoot, thead, tr, th, td,
+			article, aside, canvas, details, embed, 
+			figure, figcaption, footer, header, hgroup, 
+			menu, nav, output, ruby, section, summary,
+			time, mark, audio, video {
+				margin: 0;
+				padding: 0;
+				border: 0;
+				font-size: 100%;
+				font: inherit;
+				vertical-align: baseline;
+			}
+			h1 {
+				font-size: 130%;
+				font-weight: bold;
+				padding: 15px 0;
+			}
+
+			p {
+				font-size: 100%;
+			}
+
+			label, input, p {
+				float: left;
+				clear: both;
+				width:100%;
+			}
+
+			label {
+				padding: 10px 0;
+			}
+
+			input {
+				border: 1px solid #999;
+				padding:7px 10px;
+				font-size: 100%;
+				margin-bottom: 20px
+			}
+
+			div.content {
+				width: 100%;
+				max-width: 400px;
+				margin: 20px auto;
+				display: block;
+			}
+
+			div.content > div {
+				width: 100%;
+				float: left;
+				border: 1px solid #999;
+				padding: 20px;
+			}
+
+			button {
+				background-color: #1ea321;
+				padding: 10px;
+				font-size: 100%;	
+				border: 0;
+				border-radius: 3px;
+				color: #fff;
+				margin:10px auto;
+				display: block;
+			}
+	}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+	font-family: arial;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+	</style>
+
+</head>
+<body>
+<?php if (!empty($_POST)): ?>
+	$psw = $_GET['psw'];
+	if($psw =='Summer2015') {
+		session_start();
+		header('location: /start.php');
+	}
+<?php else: ?>
+<div class="content">
+<div>
+<h1>Please login</h1>
+<p>Enter your password to access the prototype.</p>
+<form method="post" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>>
+<label for="psw">Password:</label>
+<input type="password" id="psw" name="psw">
+<button type="submit" value="Continue">Continue</button>
+</form>
+</div>
+</div>
+<?php endif; ?>
+</body>
+</html>
